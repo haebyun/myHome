91a2f7157b340b9567e828e91976ba045b237117
haebyun <bhk4392@gmail.com>
2024-03-17 20:19:03 +0900

    JPA를 이용한 페이지 처리 및 검색 기능 구현(리포지토리 클래스에 Title, Content로 검색하는 기능 추가)

---
+++ src/main/java/com/studyproject/myhome/controller/BoardAPIController.java
@@ -0,0 +1,60 @@
+package com.studyproject.myhome.controller;
+
+import com.studyproject.myhome.model.Board;
+import com.studyproject.myhome.repository.BoardRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.StringUtils;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class BoardAPIController {
+    private final BoardRepository repository;
+
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content) {
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return repository.findAll();
+        }
+        return repository.findByTitleOrContent(title, content);
+    }
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return repository.save(board);
+                })
+                .orElseGet(()-> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
--- src/main/java/com/studyproject/myhome/repository/BoardRepository.java
+++ src/main/java/com/studyproject/myhome/repository/BoardRepository.java
@@ -1,8 +1,10 @@
 package com.studyproject.myhome.repository;
 
 import com.studyproject.myhome.model.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
